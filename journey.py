25cf7452ffcf4632cea6ab0a3ab6c045a5f8386b
Michal Töpfer <michal.topfer@gmail.com>
2022-09-12 13:21:18 +0200

    Update code submission

--- journey.py
+++ journey.py
@@ -1,9 +1,8 @@
 from typing import List
 from flask import request, redirect, Blueprint, flash
 from flask_login import login_required, current_user
-from sqlalchemy import select
 
-from db_model import Puzzle, TeamSolved, TeamArrived, ArrivalCode, SolutionCode, db
+from db_model import Puzzle, TeamSolved, TeamArrived, ArrivalCode, SolutionCode, db, PuzzlePrerequisite
 from helpers import render, get_current_puzzlehunt
 
 journey = Blueprint('journey', __name__, template_folder='templates', static_folder='static')
@@ -18,7 +17,8 @@ def index():
     team_solves_with_arrivals = TeamSolved.query\
         .filter_by(id_team=current_user.id_team)\
         .join(Puzzle)\
-        .join(TeamArrived)\
+        .join(TeamArrived,
+              (TeamSolved.id_puzzle == TeamArrived.id_puzzle) & (TeamSolved.id_team == TeamArrived.id_team))\
         .with_entities(TeamSolved, TeamArrived)\
         .all()
     team_arrivals = TeamArrived.query\
@@ -26,9 +26,8 @@ def index():
         .join(Puzzle)\
         .filter(Puzzle.id_puzzle.not_in(
             TeamSolved.query
-            .filter_by(id_team=current_user.id_team)
-            .with_entities(TeamSolved.id_puzzle))
-        )\
+                .filter_by(id_team=current_user.id_team)
+                .with_entities(TeamSolved.id_puzzle)))\
         .all()
 
     return render("index.html", solves=team_solves_with_arrivals, arrivals=team_arrivals)
@@ -38,40 +37,56 @@ def index():
 @login_required
 def submit_code():
     code = request.form["code"]
+    id_team = current_user.id_team
+
+    solved_puzzles_ids_query = TeamSolved.query\
+        .filter_by(id_team=id_team)\
+        .with_entities(TeamSolved.id_puzzle)
+    open_puzzles_ids_query = TeamArrived.query\
+        .filter_by(id_team=id_team)\
+        .filter(TeamArrived.id_puzzle.not_in(solved_puzzles_ids_query))\
+        .with_entities(TeamArrived.id_puzzle)
 
     # check solutions for open puzzles
-    # TODO: remove already solved
-    open_puzzles_ids_query = TeamArrived.query\
-        .filter_by(id_team=current_user.id_team)\
-        .with_entities(TeamArrived.id_puzzle)
     open_puzzles_solution_codes: List[SolutionCode] = SolutionCode.query\
         .filter(SolutionCode.id_puzzle.in_(open_puzzles_ids_query))\
         .all()
     for solution in open_puzzles_solution_codes:
         if solution.code == code:
-            # TODO: repeated code
-            team_solved = TeamSolved(current_user.id_team, solution.id_puzzle, solution.id_solution_code)
+            team_solved = TeamSolved(id_team, solution.id_puzzle, solution.id_solution_code)
             db.session.add(team_solved)
             db.session.commit()
             flash(f'Řešení "{solution.code}" je správně.', "success")
+            return redirect("/")
 
     # check arrival codes for not open puzzles
-    # TODO: restrict by prerequisites
     not_open_puzzles_ids_query = Puzzle.query\
         .filter_by(id_puzzlehunt=get_current_puzzlehunt())\
         .filter(Puzzle.id_puzzle.not_in(open_puzzles_ids_query))\
+        .filter(Puzzle.id_puzzle.not_in(solved_puzzles_ids_query))\
         .with_entities(Puzzle.id_puzzle)
     not_open_puzzles_codes: List[ArrivalCode] = ArrivalCode.query\
         .filter(ArrivalCode.id_puzzle.in_(not_open_puzzles_ids_query))\
         .all()
     for arrival in not_open_puzzles_codes:
         if arrival.code == code:
-            # TODO: repeated code
-            team_arrived = TeamArrived(current_user.id_team, arrival.id_puzzle, arrival.id_arrival_code)
+            prerequisites = PuzzlePrerequisite.query\
+                .filter_by(id_new_puzzle=arrival.id_puzzle)\
+                .with_entities(PuzzlePrerequisite.id_previous_puzzle)
+            fulfilled_prerequisites = TeamSolved.query\
+                .filter_by(id_team=id_team)\
+                .filter(TeamSolved.id_puzzle.in_(prerequisites))
+            if fulfilled_prerequisites.count() < prerequisites.count():
+                flash(f'Nejdřív musíte vyřešit předchozí šifry.', "danger")
+                return redirect("/")
+
+            team_arrived = TeamArrived(id_team, arrival.id_puzzle, arrival.id_arrival_code)
             db.session.add(team_arrived)
             db.session.commit()
             flash(f'Kód stanoviště "{arrival.code}" je správný.', "success")
+            return redirect("/")
 
     # TODO: check puzzlehunt global codes
 
+    flash(f'Kód "{code}" není správný (nebo už byl zadán).', "danger")
     return redirect("/")
