5e7d11a1b7da6cc05afd6cb1777db5463f698fab
Michal Töpfer <michal.topfer@gmail.com>
2022-09-13 16:23:43 +0200

    Update database creation

--- create_db.py
+++ create_db.py
@@ -1,4 +1,14 @@
+from db_model import Puzzlehunt, Settings
 from tmm_webinfo import db, app
 
 with app.app_context():
     db.create_all()
+
+    if db.session.query(Puzzlehunt).first() is None:
+        default_puzzlehunt = Puzzlehunt("Šifrovačka")
+        db.session.add(default_puzzlehunt)
+        db.session.commit()
+        current_puzzlehunt = Settings("current_puzzlehunt")
+        current_puzzlehunt.value = str(default_puzzlehunt.id_puzzlehunt)
+        db.session.add(current_puzzlehunt)
+        db.session.commit()
